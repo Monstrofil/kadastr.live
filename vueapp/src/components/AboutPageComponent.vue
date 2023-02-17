e5462c2afcb1ad93bbb21db8650e6303f37a7bf3
Oleksandr Shyshatskyi <oshyshatskyi@cloudlinux.com>
2023-02-17 20:57:26 +0200

    Added information about contacts

--- vueapp/src/components/AboutPageComponent.vue
+++ vueapp/src/components/AboutPageComponent.vue
@@ -14,20 +14,22 @@
   <div class="container text-block">
     <div class="row" style="margin-bottom: 50px;">
       <div class="col-md-12">
-        <h1>About</h1>
+        <h1>Про проєкт</h1>
         <div>
           <div class="alert alert-danger">
-            Увага! Останнє оновлення даних відбулось <b>19 лютого 2022 р. о 21:14</b>.
-            Подальші оновлення неможливі через блокування
+            Увага! Останнє оновлення даних відбулось <b>16 лютого 2023 р. о 11:26</b>.
+            Подальші оновлення не гарантуються через блокування
             <a href="http://wikimap.dzk.gov.ua/wiki/API_%D0%95-%D1%81%D0%B5%D1%80%D0%B2%D1%96%D1%81%D0%B8">API ДЗК</a>
             на час воєнного стану.<br>
             <br>
-            <strong>Звертаємо також увагу на те, що сервіс не є офіційним джерелом та не має використовуватись у професійній діяльності.</strong>
+            <strong>Звертаємо також увагу на те, що сервіс не є офіційним джерелом та не має використовуватись у
+              професійній діяльності.</strong>
           </div>
         </div>
         <p>
           Проект розроблено волонтером <a target="_blank" href="https://www.openstreetmap.org/">OpenStreetMap
-          Ukraine</a> <b>без</b> участі Міністерства цифрової трансформації та іноземних партнерів. Подяка за підтримку кавомашині та домашньому коту.
+          Ukraine</a> <b>без</b> участі Міністерства цифрової трансформації та іноземних партнерів. Подяка за підтримку
+          кавомашині та домашньому коту.
         </p>
 
         <p>
@@ -42,7 +44,8 @@
         </p>
       </div>
     </div>
-    <div class="row">
+
+    <div class="row" style="margin-bottom: 50px;">
       <div class="col-md-12">
         <h2>
           Підключення до сторонніх програм
@@ -50,21 +53,63 @@
         <p>
           Для підключення мапи до сторонніх програм доступні наступні сервіси:
         </p>
-        <p>
-          WMTS
-          <pre>
-https://kadastr.live/tiles/styles/kmplus/wmts.xml
-          </pre>
-        </p>
-        <p>
-          XYZ
-          <pre>
-https://kadastr.live/tiles/raster/styles/parcels/{z}/{x}/{y}.png
-          </pre>
-        </p>
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <div class="card w-100">
+              <div class="card-body">
+                <h5 class="card-title">WMTS</h5>
+                <p class="card-text">
+                  https://kadastr.live/tiles/raster/styles/parcels/wmts.xml
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <div class="card w-100">
+              <div class="card-body">
+                <h5 class="card-title">XYZ</h5>
+                <p class="card-text">
+                  https://kadastr.live/tiles/raster/styles/parcels/{z}/{x}/{y}.png
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
         <p>
           Публічні посилання доступні без авторизації усім бажаючим.
         </p>
+      </div>
+    </div>
+
+     <div class="row">
+      <div class="col-md-12">
+        <h2>
+          Контакти для зв'язку
+        </h2>
+        <div class="row">
+          <div class="col-xs-12 col-md-6">
+            <div class="card w-100">
+              <div class="card-body">
+                <h5 class="card-title">GitHub Issues</h5>
+                <p class="card-text">
+                  <a href="https://github.com/Monstrofil/kadastr.live/issues" target="_blank">
+                    https://github.com/Monstrofil/kadastr.live/issues
+                  </a>
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-xs-12 col-md-6">
+            <div class="card w-100">
+              <div class="card-body">
+                <h5 class="card-title">Telegram</h5>
+                <p class="card-text">
+                  <a href="https://t.me/Monstrofil" target="_blank">https://t.me/Monstrofil</a>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
